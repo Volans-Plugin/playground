a7dd744fa2251465fd681160a19b9fc7faa236a4
TheMeinerLP <p.glanz@madfix.me>
2023-04-20 08:41:18 +0200

    Setup basic paperweight project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,43 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.20"
+    id("io.papermc.paperweight.userdev") version "1.5.4"
+    id("xyz.jpenilla.run-paper") version "2.0.1" // Adds runServer and runMojangMappedServer tasks for testing
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.3" // Generates plugin.yml
 }
 
 group = "builders.volans"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://papermc.io/repo/repository/maven-public/")
     mavenCentral()
 }
 
 dependencies {
+    paperweight.paperDevBundle("1.19.4-R0.1-SNAPSHOT")
     testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("xyz.jpenilla", "reflection-remapper", "0.1.0-SNAPSHOT")
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
+}
+
+tasks {
+    assemble {
+        dependsOn("reobfJar")
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
+
+bukkit {
+    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
+    main = "builders.volans.playground.Playground"
+    apiVersion = "1.19"
+    authors = listOf("TheMeinerLP")
 }
---
+++ src/main/kotlin/builders/volans/playground/Playground.kt
@@ -0,0 +1,10 @@
+package builders.volans.playground
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Playground : JavaPlugin() {
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+}
